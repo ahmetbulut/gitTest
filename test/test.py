9a5ddcd76188cf5bf50c0c975915a902b59c1831
ahmetbulut <ahmetbulut@gmail.com>
2023-12-11 15:17:01 +0300

    CI logic fixed!

--- test/test.py
+++ test/test.py
@@ -2,10 +2,10 @@ import unittest
 from sorting import mock
 
 class MyTestCase(unittest.TestCase):
-    def test_sorting_boolean(self):
-        self.assertEqual(True, mock())  # add assertion here
+    def test_sorting_1(self):
+        self.assertEqual(None, mock())  # add assertion here
 
-    def test_sorting_none(self):
+    def test_sorting_2(self):
         self.assertEqual(None, mock())  # add assertion here
 
 if __name__ == '__main__':
