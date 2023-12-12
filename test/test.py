2efc184d934d07e1563b96029ee995b577afcf03
bulut <ahmetbulut@sehir.edu.tr>
2023-12-12 11:56:35 +0300

    A new stupid test.

--- test/test.py
+++ test/test.py
@@ -8,5 +8,8 @@ class MyTestCase(unittest.TestCase):
     def test_sorting_2(self):
         self.assertEqual(None, mock())  # add assertion here
 
+    def test_IdontWannaFollowAnyConventions(self):
+        pass
+
 if __name__ == '__main__':
     unittest.main()
